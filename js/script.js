deeb9617e67899e7d4e032c99be189c1223c7d91
gideon-hubs <oseini.gideon@gmail.com>
2023-01-04 14:24:03 +0000

    updated commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: '#app',
+    data() {
+      return {
+        slides: [
+         [
+          "https://picsum.photos/1024/480/?image=45", 
+          "https://picsum.photos/1024/480/?image=40",
+          "https://picsum.photos/1024/480/?image=47"
+         ],
+         [
+          "https://picsum.photos/1024/480/?image=48", 
+          "https://picsum.photos/1024/480/?image=39",
+          "https://picsum.photos/1024/480/?image=55"
+         ],
+         [
+          "https://picsum.photos/1024/480/?image=51", 
+          "https://picsum.photos/1024/480/?image=52",
+          "https://picsum.photos/1024/480/?image=53"
+         ]
+        ]
+      }
+    }
+  })
